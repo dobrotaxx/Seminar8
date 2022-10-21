57654a15f036c4f3128f821d7d2edb56e0c66290
Semyon <tonkih.sem@mail.ru>
2022-10-21 22:54:21 +0500

    added DZ01

---
+++ DZ01/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] GenerateArray(int height, int width, int deviation)
+{
+    int[,] generatedArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            generatedArray[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return generatedArray;
+}
+
+void PrintColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void ShowArray(int[,] inputArray)
+{
+    PrintColorData($" \t");
+    for (int i = 0; i < inputArray.GetLength(1); i++)
+    {
+        PrintColorData($"{i}\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        PrintColorData($"{i}\t");
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write($"{inputArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortingTheRowInDescendingOrder(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int bigger = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i,k] > array[i, bigger])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, bigger];
+                    array[i, bigger] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] generatedArray = GenerateArray(5, 5, 20);
+ShowArray(generatedArray);
+int[,] result = SortingTheRowInDescendingOrder(generatedArray);
+ShowArray(result);
+
+
+
